9b4cdb73404db930cc9d6d6e23a8fe8f4588d657
Heekei <zxq0610@qq.com>
2020-04-14 18:26:41 +0800

    优化判断

--- lazyLoad.js
+++ lazyLoad.js
@@ -7,68 +7,42 @@
 (function () {
     var lazyLoad = {
         imgs: [],
-        arrImgs: [],
-        scrollDelay: 100 //滚动节流间隔
+        waitThrottle: 200,
+        scrollDelay: 300 //滚动节流间隔
         , watch: function (doms) {
-            doms = doms.filter(function (element, index) {
-                if (!element._isloaded && lazyLoad.eleInView(element)) {
-                    element.src = element.getAttribute("data-src");//显示图片
-                    // doms.splice(index, 1);
-                    element._isloaded = true;
-                    return false;
+            for (var i = 0; i < doms.length; i++) {
+                if (!doms[i]._isloaded && lazyLoad.eleInView(doms[i])) {
+                    doms[i].src = doms[i].getAttribute("data-src");//显示图片
+                    doms[i]._isloaded = true;
                 }
-                return true;
-            }, this);
+            }
         }
         , eleInView: function (elem) {
             var bounding = elem.getBoundingClientRect();
+            var h = (window.innerHeight || document.documentElement.clientHeight);
+            var w = (window.innerWidth || document.documentElement.clientWidth);
             return (
-                bounding.top >= 0 &&
-                bounding.left >= 0 &&
-                bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-                bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+                ((bounding.top >= 0 && bounding.top <= h) || (bounding.bottom >= 0 && bounding.bottom <= h)) &&
+                ((bounding.left >= 0 && bounding.left <= w) || (bounding.right >= 0 && bounding.right <= w))
             );
-        }/* function (el) {
-            var top = el.offsetTop,
-                left = el.offsetLeft,
-                width = el.offsetWidth,
-                height = el.offsetHeight;
-
-            while (el.offsetParent) {
-                el = el.offsetParent;
-                top += el.offsetTop;
-                left += el.offsetLeft;
-            }
-
-            return (
-                top <= (window.pageYOffset + window.innerHeight) &&
-                left <= (window.pageXOffset + window.innerWidth) &&
-                (top + height) >= window.pageYOffset &&
-                (left + width) >= window.pageXOffset
-            );
-        } */
+        }
     };
     var timer = null;
+    var scrollTimer = null;
     document.addEventListener('DOMContentLoaded', function () {
         lazyLoad.imgs = document.querySelectorAll('img[data-src]'); //document.getElementsByTagName("img");//HTMLCollection
-        for (var len = 0; len < lazyLoad.imgs.length; len++) {
-            lazyLoad.arrImgs.push(lazyLoad.imgs[len]);
-        }
-        lazyLoad.watch(lazyLoad.arrImgs);
-        timer = setInterval(function () {
-            lazyLoad.watch(lazyLoad.arrImgs);
-        }, 500);
+        lazyLoad.watch(lazyLoad.imgs);
         function scrollThrottle() {
             window.removeEventListener("scroll", scrollThrottle, false);
             window.removeEventListener("resize", scrollThrottle, false);
             clearInterval(timer);
-            setTimeout(function () {
+            clearInterval(scrollTimer);
+            scrollTimer = setTimeout(function () {
                 window.addEventListener("scroll", scrollThrottle, false);
                 window.addEventListener("resize", scrollThrottle, false);
-                lazyLoad.watch(lazyLoad.arrImgs);
-                timer = setInterval(function () {
-                    lazyLoad.watch(lazyLoad.arrImgs);
-                }, 500);
+                timer = setTimeout(function () {
+                    lazyLoad.watch(lazyLoad.imgs);
+                }, lazyLoad.waitThrottle);
             }, lazyLoad.scrollDelay);
         }
         window.addEventListener("scroll", scrollThrottle, false);
